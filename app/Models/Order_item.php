9b50a7de1f5afdf6b4850eb6295f3e76d8a9e023
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-28 15:01:54 +0330

    create model

---
+++ app/Models/Order_item.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order_item extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'order_id',
+        'variation_id',
+
+        'quantity',
+        'price',
+        'offer',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    //* Relations
+
+    /**
+     * Get the order that owns the order item.
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * Get the variation that owns the order item.
+     */
+    public function variation()
+    {
+        return $this->belongsTo(Variation::class);
+    }
+
+}
