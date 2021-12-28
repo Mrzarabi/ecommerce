b9a327554a8711575ff63ebea5abd1358e46cd54
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-28 13:04:02 +0330

    write db seeder and seed all data in mysql successfully

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Discount;
+use App\Models\Feature;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Variation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +21,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = User::factory(10)->create();
+
+        $categories = Category::factory(10)->create();
+
+        $brands = Brand::factory(15)->create();
+
+        $disounts = Discount::factory(12)->create();
+
+        $categories->each(function($category) use($brands, $disounts) {
+            $category->products()->saveMany(Product::factory(5)->create([
+                'brand_id' => $brands->random()->id,
+                'discount_id' => $disounts->random()->id,
+                'category_id' => $category->id
+            ]))->each(function($product) {
+                $product->images()->saveMany(Image::factory(3)->create([
+                    'product_id' => $product->id
+                ]));
+                $product->variations()->saveMany(Variation::factory(4)->create([
+                    'product_id' => $product->id
+                ]))->each(function($variation) {
+                    $variation->features()->saveMany(Feature::factory(5)->create([
+                        'variation_id' => $variation->id
+                    ]));
+                });
+            });
+        });
     }
 }
