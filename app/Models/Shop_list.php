a0babd0ed774f450a0b819d44c2c99bd3a33e59e
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-28 15:02:27 +0330

    create model shop list

---
+++ app/Models/Shop_list.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop_list extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'variation_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    //* Realtions
+
+    /**
+     * Get the user that owns the shop list.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    /**
+     * Get the variation that owns the shop list.
+     */
+    public function variation()
+    {
+        return $this->belongsTo(Variation::class);
+    }
+}
